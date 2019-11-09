c99e63cb3cdf53c6fb0909264064aa036ae9f0ba
Anvi Puri <anvipuri@gmail.com>
2019-11-09 16:25:47 +0530

    String practice

--- Strings.py
+++ Strings.py
@@ -6,8 +6,21 @@ phrase2="I have been coding since 4 years."
 print(phrase+phrase2)
 
 #String functions
-print(phrase.lower())
+lower_phrase=phrase.lower()
 print(phrase2.islower())
+print(lower_phrase.islower())
+
+print(len(phrase))
+
+print(phrase[0])
+
+print(phrase.index("coder"))
+
+print(phrase2.replace("4","10"))
+
+
+
+
 
 
 
