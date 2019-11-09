3c9e9338bf16fcd594d35f4d3810df5210bedbfb
Anvi Puri <anvipuri@gmail.com>
2019-11-09 16:39:43 +0530

    Functions on numbers

---
+++ Numbers.py
@@ -0,0 +1,32 @@
+from math import *
+
+print(3.1+8.9)
+print(10%3)
+print(10**3)
+
+#converitng a nukber to a string
+print(str(40)+ " is my favourite number")
+
+#absolute value of a number
+a=-10
+print(abs(a))
+
+#power of a number
+b=5
+print(pow(a,b))
+
+#max,min of numbers
+print(max(a,b))
+print(min(a,b))
+
+#rounding a number
+print(round(3.789))
+
+#floor method
+print(floor(6.78))
+
+#ceil method
+print(ceil(3.1))
+
+#square root of a number
+print(sqrt(100))
