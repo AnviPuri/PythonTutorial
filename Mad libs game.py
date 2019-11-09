64921574873640dd1d7c09ed83bc49aa6ef4b2f5
Anvi Puri <anvipuri@gmail.com>
2019-11-09 17:19:15 +0530

    Basic calculator and Madlibs game

---
+++ Mad libs game.py
@@ -0,0 +1,7 @@
+name=input("Enter a name")
+name2=input("Enter another name")
+action=input("What are you doing?")
+
+print("Monkeys are like " + name)
+print("Donkeys are like " + name2)
+print("I like to " + action)
