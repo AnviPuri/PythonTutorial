183682ac399dd01e693548a90506740773dcadba
Anvi Puri <anvipuri@gmail.com>
2019-11-09 16:13:51 +0530

    Shape.py

---
+++ Strings.py
@@ -0,0 +1,20 @@
+phrase="I am a coder."
+print(phrase)
+
+#concatenation
+phrase2="I have been coding since 4 years."
+print(phrase+phrase2)
+
+#String functions
+print(phrase.lower())
+print(phrase2.islower())
+
+
+
+
+
+
+
+
+
+
