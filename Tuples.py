65bf8fd443acf6ea36308ffde6d051a68d0eebc3
Anvi Puri <anvipuri@gmail.com>
2019-11-09 18:19:36 +0530

    Tuples

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+#Creating a tuple
+#Tuples are immutable
+primeNumbers=(2,3,5,7,9)
+print(primeNumbers)
+print(primeNumbers[3])
+print(primeNumbers[0:2])
+
+coordinates=[(4,5),(6.7),(8,9)]
+print(coordinates)
+print(coordinates[2])
