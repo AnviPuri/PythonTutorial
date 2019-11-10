f5baaeb8c8a98451d2f7d658e2f3274966e002aa
Anvi Puri <anvipuri@gmail.com>
2019-11-10 09:02:12 +0530

    Improved calculator

--- BasicCalculator.py
+++ BasicCalculator.py
@@ -1,4 +1,18 @@
-num1=input("Enter the first number:")
-num2=input("Enter the second number:")
-result=int(num1)+int(num2)
-print("The sum is " + str(result))
+def calculator(num1,num2,op):
+    if op == "+":
+        print(num1+num2)
+    elif op == "-":
+        print(num1-num2)
+    elif op == "*":
+        print(num1*num2)
+    elif op == "/":
+        print(num1/num2)
+    else:
+        print("Choose the right operator")
+
+
+
+num1=float(input("Enter the first number:"))
+num2=float(input("Enter the second number:"))
+op=input("Enter the operator: ( + , - , / , *)")
+calculator(num1,num2,op)
