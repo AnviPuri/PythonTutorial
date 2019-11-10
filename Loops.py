375c070adeba91984df664fa934f6f43c5c46630
Anvi Puri <anvipuri@gmail.com>
2019-11-10 12:24:28 +0530

    nested loops and 2d loops

--- Loops.py
+++ Loops.py
@@ -24,3 +24,22 @@ for index in range(10,20):
 
 for index in range(len(array)):
     print(array[index])
+
+#2D Lists and nested loops
+#2D lists
+numberSets=[
+    [1, 2, 3],
+    [2, 4, 6],
+    [3, 6, 9]
+]
+
+print(numberSets[0][0]+numberSets[1][1]+numberSets[2][2])
+
+#nested loops
+for row in numberSets:
+    print(row)
+
+for row in numberSets:
+    for col in row:
+        print(col)
+
