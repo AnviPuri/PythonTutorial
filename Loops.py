ded62c321e09eb2874ca1c756c74c7b3aa5fc1db
Anvi Puri <anvipuri@gmail.com>
2019-11-10 11:57:57 +0530

    loops

---
+++ Loops.py
@@ -0,0 +1,26 @@
+#while loop
+i = 1
+
+while i <= 10:
+    print(i)
+    i=i+1
+
+print("Outside the loop")
+
+#for loop
+array= [1,2,3,4,"Jamaica"]
+
+for i in array:
+    print(i)
+
+for j in "Miami":
+    print(j)
+
+for index in range(10):
+    print(index)
+
+for index in range(10,20):
+    print(index)
+
+for index in range(len(array)):
+    print(array[index])
