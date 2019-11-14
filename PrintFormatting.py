e9817fbc46c3baabde5a3d641194bdbc5e84137b
Anvi Puri <anvipuri@gmail.com>
2019-11-14 22:10:49 +0530

    print formatting

---
+++ PrintFormatting.py
@@ -0,0 +1,15 @@
+#.format method
+name = "Anvi"
+age=22
+
+#1st method
+print("My name is {} and my age is {}".format(name,age))
+
+#2nd method
+print("My name is {n} and my age is {a}".format(n=name,a=age))
+
+salary=10000.3452
+print("My salary is {0:.1f}".format(salary))
+
+#fstring method
+print(f"My name is {name} and I am {age} years old")
